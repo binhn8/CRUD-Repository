30133e2d9483779b56a4278f2b81c4824ed0280c
Binh Nguyen <binh.nguyen@printerpix.co.uk>
2022-12-02 00:06:17 +0000

    Add project files.

---
+++ CRUD-Repository/Data/BookContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CRUD_Repository.Models;
+
+namespace CRUD_Repository.Data
+{
+    public class BookContext : DbContext
+    {
+        public BookContext (DbContextOptions<BookContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Author> Authors { get; set; }
+    }
+}
---
+++ CRUD-Repository/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRUD_Repository.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Published { get; set; }
+        public Author? Author { get; set; }
+        public Category? Category { get; set; }
+        public int AuthorId { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
